4429f59e0f9c4953ad23e7e6daeb8ab27d58bb5b
Adair M <adairmtz13@hotmail.com>
2025-06-29 03:59:48 -0600

    update

--- src/main/java/com/repoUnsij/auth/entites/Carreras.java
+++ src/main/java/com/repoUnsij/auth/entites/Carreras.java
@@ -1,39 +1,32 @@
 package com.repoUnsij.auth.entites;
 
+import java.util.List;
+
 import com.fasterxml.jackson.annotation.JsonBackReference;
+import com.fasterxml.jackson.annotation.JsonManagedReference;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.ToString;
 
-@Entity (name = "usuarios")
+@Entity
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 @ToString
-public class Usuarios {
-    
+public class Carreras {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+    private Long id;   
     private String nombre;
-    private String correo;
-    private String contrasenia;
-    private Boolean activo;
 
-    @ManyToOne
-    @JoinColumn(name = "rol_id")
-    private Roles rol;
-
-    @ManyToOne
-    @JoinColumn(name = "carrera_id")
-    private Carreras carrera;
-
+    @OneToMany(mappedBy = "carrera")
+    @JsonBackReference
+    private List<Usuarios> usuarios;
 }
